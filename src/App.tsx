d887b3d08c42c55f46c8921a828bb3857770390c
Yachim <jachym.kohout@gmail.com>
2022-11-20 14:51:59 +0100

    Add state variable of cells that gets changed on rowCnt or colCnt change

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,49 @@ import { useEffect, useState } from 'react'
 import { Cell } from './types';
 
 function App() {
-    const [width, setWidth] = useState(10);
-    const [height, setHeight] = useState(10)
+    const [rowCnt, setRowCnt] = useState(5);
+    const [colCnt, setColCnt] = useState(5)
     
-    const [cells, setCells] = useState<Cell[][]>();
+    const [cells, setCells] = useState<Cell[][]>(
+        Array(rowCnt).fill(
+            Array(colCnt).fill("empty")
+        )
+    );
 
     useEffect(() => {
-    }, [width, height])
+        setCells(prev => {
+            const prevRowCnt = prev.length;
+            const prevColCnt = prev[0].length;
+            
+            const diffRowCnt = rowCnt - prevRowCnt;
+            const diffColCnt = colCnt - prevColCnt;
+            
+            if (diffRowCnt < 0) {
+                prev = prev.slice(0, rowCnt);
+            }
+            else {
+                prev = [
+                    ...prev,
+                    ...Array(diffRowCnt).fill(
+                        Array(colCnt).fill("empty")
+                    )
+                ];
+            }
+
+            if (diffColCnt < 0) {
+                prev = prev.map(row => row.slice(0, colCnt));
+            }
+            else {
+                prev = prev.map(row => [
+                    ...row,
+                    ...Array(diffColCnt).fill("empty")
+                ]);
+            }
+
+            console.log(prev)
+            return prev;
+        })
+    }, [rowCnt, colCnt])
 
     return (
         <div className="App">
